4cd252fb88de7e3b3b3597a00e3e44904cfdf283
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-26 14:31:30 -0300

    feat<backend>: add cors to the app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
+import cors from '@fastify/cors';
 import fastify from 'fastify';
 import { rideRoutes } from './routes/ride';
 
 export const app = fastify();
+
+app.register(cors);
 
 app.register(rideRoutes, { prefix: '/ride' });
