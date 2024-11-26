10ae79545e62cba0ad24af520bcb62834dc7e94a
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-26 18:13:19 -0300

    refactor<frontend>: show EstimateRidePage and Header on App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,11 @@
+import { Header } from './components/Headers/Header/Header';
+import { EstimateRidePage } from './pages/EstimateRidePage';
+
 export const App = () => {
-  return <>oi wolfi</>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <EstimateRidePage />
+    </div>
+  );
 };
