9838dc1eafff9dc0a0e656e3ff160bc53b481bea
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-27 18:13:28 -0300

    feat<frontend>: create message error for the FormRideHistorySchema

--- frontend/src/components/Forms/FormRideHistory/schema.ts
+++ frontend/src/components/Forms/FormRideHistory/schema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export const FormRideHistorySchema = z.object({
-  customer_id: z.string(),
+  customer_id: z
+    .string()
+    .min(1, { message: 'O id do cliente precisa ter pelo menos 1 caractere' }),
   driver_id: z
     .string()
     .optional()
