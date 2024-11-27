39115422742bc89975717a54485832f93c76a18d
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-27 14:44:35 -0300

    feat<frontend>: create route for estimate page and history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router';
 import { Header } from './components/Headers/Header/Header';
 import { EstimateRidePage } from './pages/EstimateRidePage';
 
@@ -5,7 +6,16 @@ export const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <EstimateRidePage />
+      <Routes>
+        <Route
+          path="/"
+          element={<EstimateRidePage />}
+        />
+        <Route
+          path="/history"
+          element={<h1>History</h1>}
+        />
+      </Routes>
     </div>
   );
 };
