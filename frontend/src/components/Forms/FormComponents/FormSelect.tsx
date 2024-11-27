b3105443883c188022b95d9d02ea6925147da0d2
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-27 17:15:31 -0300

    feat<frontend>: create FormSelect to FormComponents

---
+++ frontend/src/components/Forms/FormComponents/FormSelect.tsx
@@ -0,0 +1,68 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { SelectProps } from '@radix-ui/react-select';
+import { useFormContext } from 'react-hook-form';
+
+type FormsSelectProps = SelectProps & {
+  label?: string;
+  name: string;
+  placeholder: string;
+  options: Array<{ label: string; value: string }>;
+};
+
+export const FormsSelect = ({
+  name,
+  label,
+  placeholder,
+  options,
+  ...rest
+}: FormsSelectProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+          <Select
+            onValueChange={field.onChange}
+            defaultValue={field.value}
+            name={field.name}
+            {...rest}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {options.map((opt) => (
+                <SelectItem
+                  key={`opt-value-${name}-${opt.value}`}
+                  value={opt.value}
+                >
+                  {opt.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
--- frontend/src/components/Forms/FormComponents/index.ts
+++ frontend/src/components/Forms/FormComponents/index.ts
@@ -1,9 +1,11 @@
 import { Form } from '@/components/ui/form';
 import { FormsInput } from './FormInput';
+import { FormsSelect } from './FormSelect';
 import { FormsSubmit } from './FormSubmit';
 
 export const FormComponents = {
   Base: Form,
   Input: FormsInput,
+  Select: FormsSelect,
   Submit: FormsSubmit,
 };
