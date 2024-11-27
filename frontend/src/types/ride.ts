57fbdccb40565646daf971e074de72399015fe4a
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-27 17:20:17 -0300

    feat<frontend>: create type for Ride

---
+++ frontend/src/types/ride.ts
@@ -0,0 +1,13 @@
+export type Ride = {
+  id: number;
+  date: string;
+  origin: string;
+  destination: string;
+  distance: number;
+  duration: string;
+  driver: {
+    id: number;
+    name: string;
+  };
+  value: number;
+};
