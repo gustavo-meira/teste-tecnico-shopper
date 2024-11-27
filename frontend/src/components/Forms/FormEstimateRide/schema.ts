7466055d50328e8f0a00cdc3a35811388743605a
Gustavo Meira <gmeira.antonio@gmail.com>
2024-11-27 18:13:16 -0300

    feat<frontend>: create message error for the FormEstimateRideSchema

--- frontend/src/components/Forms/FormEstimateRide/schema.ts
+++ frontend/src/components/Forms/FormEstimateRide/schema.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod';
 
 export const formEstimateRideSchema = z.object({
-  customer_id: z.string(),
-  origin: z.string(),
-  destination: z.string(),
+  customer_id: z.string().min(1, {
+    message: 'O id do cliente precisa ter pelo menos 1 caractere',
+  }),
+  origin: z
+    .string()
+    .min(1, { message: 'A origem precisa ter pelo menos 1 caractere' }),
+  destination: z
+    .string()
+    .min(1, { message: 'O destino precisa ter pelo menos 1 caractere' }),
 });
 
 export type FormEstimateRideSchemaInput = z.input<
